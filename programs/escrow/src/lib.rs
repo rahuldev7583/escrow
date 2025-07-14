270b319ac2ee7e7c7cc9116a0a78effe73b44f14
rahuldev7583 <rahuldev7583@gmail.com>
2025-07-15 03:24:04 +0530

    make and refund proram added

--- programs/escrow/src/lib.rs
+++ programs/escrow/src/lib.rs
@@ -14,8 +14,4 @@ declare_id!("DL7B4qVtAgorNJuvt6z3PgtU2k1AUfaEFY8k9Bnxc9Vt");
 #[program]
 pub mod escrow {
     use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
-    }
 }
