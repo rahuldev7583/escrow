2b4e52d32a3b72873be4b370028b44f9d0530abb
rahuldev7583 <rahuldev7583@gmail.com>
2025-07-15 03:11:43 +0530

    initial commit

---
+++ programs/escrow/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("DL7B4qVtAgorNJuvt6z3PgtU2k1AUfaEFY8k9Bnxc9Vt");
+
+#[program]
+pub mod escrow {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
